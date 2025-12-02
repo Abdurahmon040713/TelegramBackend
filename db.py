0dfca1cff50bcd8bcb149192a47261b253933100
Abdurahmon G'ofurov <168077588+Abdurahmon040713@users.noreply.github.com>
2025-12-02 13:00:57 +0500

    salom

--- db.py
+++ db.py
@@ -1,1 +1,2 @@
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/telegram_app"
+#DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/telegram_app"
+DATABASE_URL = "sqlite:///./telegram_app.db"
